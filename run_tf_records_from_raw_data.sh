f2c75c05aea75e25432db7440da2ba24be5d402b
Reshinth Adithyan <36307201+reshinthadithyan@users.noreply.github.com>
2021-10-10 20:16:34 +0530

    Adding .sh script to run create-tf-record script

---
+++ run_tf_records_from_raw_data.sh
@@ -0,0 +1,7 @@
+cd ..
+python mesh-transformer-jax/create_finetune_tfrecords.py --output-dir "records_data_dir/train" --model_name "gpt2" --log_idt "train" "raw_data_dir/train"  "code_clippy"
+echo "train split records generated..."
+python mesh-transformer-jax/create_finetune_tfrecords.py --output-dir "records_data_dir/eval" --model_name "gpt2" --log_idt "eval" "raw_data_dir/train"  "code_clippy"
+echo "eval split records generated..."
+python mesh-transformer-jax/create_finetune_tfrecords.py --output-dir "records_data_dir/test" --model_name "gpt2" --log_idt "test" "raw_data_dir/test"  "code_clippy"
+echo "test split records generated..."
