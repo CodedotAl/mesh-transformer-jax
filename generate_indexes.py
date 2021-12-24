895f6e0f320e7f4219d44c8b4b0e22258852e2e7
Erfan Al-Hossami <ealhossa@uncc.edu>
2021-12-24 02:12:12 -0500

    Added loading from bucket

--- generate_indexes.py
+++ generate_indexes.py
@@ -1,7 +1,7 @@
 import argparse
 
 from pathlib import Path
-
+import os
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--gs_project_id",
@@ -15,11 +15,13 @@ parser.add_argument(
     "--output_dir", type=str, help="Where the indexes will be stored locally."
 )
 
+parser.add_argument ("--load_bucket", type=bool, help="Whether to load the folder structure from the Bucket directly", default = False, nargs = '?')
+
 args = parser.parse_args()
 input_dir = Path(args.input_dir)
 root_dir = input_dir.name
 output_dir = Path(args.output_dir)
-
+load_bucket = args.load_bucket
 # get the list of tfrecords
 # train_tfrecords = [
 #     str(f)
@@ -32,18 +34,23 @@ output_dir = Path(args.output_dir)
 #     if "valid" in str(f)
 # ]
 # construct index file paths in the format of a gs bucket
-train_indexes = [
-    f"gs://{args.gs_project_id}/{root_dir}/{str(f).split(f'/{root_dir}/')[-1]}"
-    for f in input_dir.glob("**/*.tfrecords")
-    if "train" in str(f)
-]
-print(train_indexes[:5])
-val_indexes = [
-    f"gs://{args.gs_project_id}/{root_dir}/{str(f).split(f'/{root_dir}/')[-1]}"
-    for f in input_dir.glob("**/*.tfrecords")
-    if "valid" in str(f)
-]
+if load_bucket  == False:
+    train_indexes = [
+                        f"gs://{args.gs_project_id}/{root_dir}/{str(f).split(f'/{root_dir}/')[-1]}"
+                        for f in input_dir.glob("**/*.tfrecords")
+                        if "train" in str(f)
+                    ]
+    print(train_indexes[:5])
+    val_indexes = [
+                        f"gs://{args.gs_project_id}/{root_dir}/{str(f).split(f'/{root_dir}/')[-1]}"
+                        for f in input_dir.glob("**/*.tfrecords")
+                        if "valid" in str(f)
+                        ]
 
+else:
+    list_files = os.popen(f'gsutil ls -r gs://{args.gs_project_id}/{root_dir}').read().split('\n')
+    train_indexes = [f for f in list_files if 'train' in str(f) and '.tfrecords' in str(f)]
+    val_indexes = [f for f in list_files if 'valid' in str(f) and '.tfrecords' in str(f)]
 with open(output_dir / "code_clippy.train.index", "w") as f:
     f.write("\n".join(train_indexes))
 
